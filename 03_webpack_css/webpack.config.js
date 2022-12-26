490722659f34389ad03692959501bb133989d91d
finley <FinleyLu@trevi.cc>
2022-12-26 14:52:27 +0800

    add `css-loader` to process `css` files without any error.

--- 03_webpack_css/webpack.config.js
+++ 03_webpack_css/webpack.config.js
@@ -1,3 +1,23 @@
 module.exports = {
 	entry: "./src/index.js",
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				// 1.
+				// loader: 'css-loader'
+
+				// 2.
+				// use: [
+				// 	'css-loader'
+				// ]
+
+				// 3.
+				use: [
+					{ loader: 'css-loader', /** other options */}
+				]
+			}
+		]
+	}
 }
