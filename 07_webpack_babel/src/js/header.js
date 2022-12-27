ac49565089374c69142fada5b767e1f5fee9342f
finley <FinleyLu@trevi.cc>
2022-12-27 13:26:10 +0800

    webpack_bable init.

---
+++ 07_webpack_babel/src/js/header.js
@@ -0,0 +1,12 @@
+
+console.log('header.js bundled.');
+import '../css/font.css'
+import '../css/style.css'
+import '../css/style.scss'
+
+const h = document.createElement('h1')
+
+h.className = 'title'
+h.innerHTML = 'Hello Webpack.'
+
+document.body.appendChild(h)
---
+++ 07_webpack_babel/src/js/math.js
@@ -0,0 +1,4 @@
+// ESModule
+export function sum(num1, num2) {
+	return num1 + num2
+}
